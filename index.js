70f2b1815da07b9132e16be7ef41e77fa7555fb3
Rohith Kumar Reddy <111877972+RohithReddy-1402@users.noreply.github.com>
2024-11-11 16:06:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors'); // Import the cors middleware
+const shortnerRouter = require('./Routes/shortnerRoute');
+const {connectDB } = require('./db');
+
+const app = express()
+
+app.use(cors({
+    origin: "*",
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  }));
+
+app.use(express.json())
+
+
+connectDB()
+.then(() => {
+    app.listen(8000 , () => {
+        console.log(`server is running on https://localhost:8000`);
+    })
+}).catch((err) => {
+    console.log("Mongo DB connection failed :",err);
+})
+
+
+app.use('/api/v1/url',shortnerRouter)
